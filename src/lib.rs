ecb5ab3f7913751b4b4e280f5c3808795ee159bc
Garrett van Wageningen <ghvanw283@gmail.com>
2024-08-23 16:34:32 -0500

    messing around - trying different ownership

--- src/lib.rs
+++ src/lib.rs
@@ -1,59 +1,175 @@
-use std::string::String;
+use std::{borrow::Borrow, string::String};
 
-pub enum TokenType {
-    SemiColon,
-    Book,
-    Number 
+pub enum Token {
+    Colon,
+    Dash,
+    Number,
+    Book(String),
 }
 
-pub struct ParseState {
+pub fn is_bible_book(input: &str) -> bool {
+    let it = input.to_lowercase();
+    it == "1 Chronicles"
+    || it == "1 Corinthians"
+    || it == "1 John"
+    || it == "1 Kings"
+    || it == "1 Peter"
+    || it == "1 Samuel"
+    || it == "1 Thessalonians"
+    || it == "1 Timothy"
+    || it == "2 Chronicles"
+    || it == "2 Corinthians"
+    || it == "2 John"
+    || it == "2 Kings"
+    || it == "2 Peter"
+    || it == "2 Samuel"
+    || it == "2 Thessalonians"
+    || it == "2 Timothy"
+    || it == "3 John"
+    || it == "Acts"
+    || it == "Amos"
+    || it == "Colossians"
+    || it == "Daniel"
+    || it == "Deuteronomy"
+    || it == "Ecclesiastes"
+    || it == "Ephesians"
+    || it == "Esther"
+    || it == "Exodus"
+    || it == "Ezekiel"
+    || it == "Ezra"
+    || it == "Galatians"
+    || it == "Genesis"
+    || it == "Habakuk"
+    || it == "Haggai"
+    || it == "Hebrews"
+    || it == "Hosea"
+    || it == "Isaiah"
+    || it == "James"
+    || it == "Jeremiah"
+    || it == "Job"
+    || it == "Joel"
+    || it == "John"
+    || it == "Jonah"
+    || it == "Joshua"
+    || it == "Jude"
+    || it == "Judges"
+    || it == "Lamentations"
+    || it == "Leviticus"
+    || it == "Luke"
+    || it == "Malachi"
+    || it == "Mark"
+    || it == "Matthew"
+    || it == "Micah"
+    || it == "Nahum"
+    || it == "Nehemiah"
+    || it == "Numbers"
+    || it == "Philemon"
+    || it == "Philippians" 
+    || it == "Proverbs"
+    || it == "Psalms"
+    || it == "Revelation"
+    || it == "Romans"
+    || it == "Ruth"
+    || it == "Songs of Solomon"
+    || it == "Titus"
+    || it == "Zechariah"
+    || it == "Zephaniah"
+}
+
+
+pub struct ParseState<'a> {
     pub start: usize,
     pub current: usize,
-    pub line: i32
+    pub line: i32,
+    pub input: &'a str
 }
 
-impl ParseState {
-    pub fn new(start: usize, current: usize, line: i32) -> Self {
+impl<'a> ParseState<'a> {
+    pub fn new(input: &'a str) -> Self {
         ParseState {
-            start, 
-            current, 
-            line
+            start: 0, 
+            current: 0, 
+            line: 0,
+            input: input
         }
     }
 
-    pub fn advance(&self) -> Self {
+    pub fn advance(self) -> Self {
         ParseState {
             current: self.current + 1,
             ..self
         }
     }
 
-    pub fn advance_line(&self) -> Self {
+    pub fn advance_to(self, index: usize) -> Self {
         ParseState {
-            current: self.current + 1,
-            line: self.line + 1,
-            start: self.start
+            current: index,
+            ..self
         }
     }
 
-    pub fn get_current_selection(&self, input: &str) -> String {
-        input[self.start..self.current].to_string()
+    pub fn advance_line(self) -> Self {
+        ParseState {
+            current: self.current + 1,
+            line: self.line + 1,
+            start: self.start,
+            input: self.input
+        }
     }
 
-    pub fn is_at_end(&self, input: &str) -> bool {
-        self.current >= input.len()
+    pub fn get_current_selection(&self) -> &str {
+        &self.input[self.start..=self.current]
     }
 
+    pub fn get_selection_up_to(&self, next: usize) -> &str {
+        &self.input[self.start..next]
+    }
 
-    pub fn current_char(&self, input: &str) -> char {
-        input.[self.current]
+    pub fn get_selection_through(&self, next: usize) -> &str {
+        &self.input[self.start..=next]
+    }
+
+    pub fn is_at_end(&self) -> bool {
+        self.current >= self.input.len()
+    }
+
+    pub fn token_complete(&self, index: usize) -> Self {
+        ParseState {
+            start: index,
+            current: index,
+            line: self.line,
+            input: self.input
+        }
+    }
+
+    pub fn check_for_token(self, next_in: (usize, char)) -> (Option<Token>, ParseState<'a>) {
+        match next_in.1 {
+            ':' => (Some(Token::Colon), self.token_complete(next_in.0)),
+            '-' => (Some(Token::Dash), self.token_complete(next_in.0)),
+            ' ' => {
+                let sub_str = self.get_current_selection();
+
+                match sub_str {
+                    "1" => (None, self.advance_to(next_in.0)),
+                    "2" => (None, self.advance_to(next_in.0)),
+                    _ => {
+                        if is_bible_book(sub_str) {
+                            (Some(Token::Book(sub_str.to_string())), self.token_complete(next_in.0))
+                        } else {
+                            (None, self.advance_to(next_in.0))
+                        }
+                    }
+                }
+            },
+            _ => (None, self.advance_to(next_in.0))
+        }
     }
 }
 
 
 pub struct Lines {
-    start: u32,
-    end: Option<u32>
+    pub start: u32,
+    pub end: Option<u32>
 }
 
 impl Lines {
@@ -69,17 +185,23 @@ pub struct Verse {
     pub lines: Lines
 }
 
-pub fn is_at_end(state: &ParseState, input: &str) -> bool {
-    state.current >= input.len()
-}
 
 
-pub fn current_char(state: &ParseState, input: &str) -> char {
-    input[state.current]
-}
 
-pub fn scan(input: &str) -> Vec<Verse> {
+pub fn scan(input: &str) -> Vec<Token> {
+    let mut state: ParseState = ParseState::new(input);
 
+    let mut tokens = Vec::new();
+
+    for i in input.char_indices() {
+        let (it, next_state) = state.check_for_token(i);
+        if let Some(result) = it {
+            tokens.push(result);
+        }
+        state = next_state;
+    }
+
+    tokens
 }
 
 
